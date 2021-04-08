8dd24e15904b925eb6502916cae3bc7a4296b455
Christian Ferndz <christianferndz@gmail.com>
2021-04-08 01:41:14 -0500

    first commit

---
+++ src/main/java/com/iciencia/SpringBootKafkaApplication.java
@@ -0,0 +1,84 @@
+package com.iciencia;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@SpringBootApplication
+public class SpringBootKafkaApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootKafkaApplication.class);
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	// CONSUMO NORMAL DE MENSAJE
+//	@KafkaListener(topics = "TutorialTopic", groupId = "iciencia-group")
+//	public void listen(String message) {
+//		log.info("Mensaje Kafka {}", message);
+//	}
+
+	/**
+	 * El nombre container proviene desde el nombre de la clase de configuracion
+	 * 
+	 * @param message
+	 */
+	@KafkaListener(topics = "TutorialTopic", containerFactory = "kafkaListenerContainerFactory", groupId = "iciencia-group", properties = {
+			"max.poll.interval.ms:4000", "max.poll.records:10" })
+	public void listen(List<String> message) {
+		log.info("Start reading messages");
+
+		for (String string : message) {
+			log.info("Mensaje leido = {}", string);
+		}
+
+		log.info("Batch complete");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaApplication.class, args);
+	}
+
+	/**
+	 * Manejon de callback aviso de mensaje recibido.
+	 * 
+	 * Para envio de mensajes sincronos recordar el uso de .get en send
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+		for (int i = 0; i < 100; i++) {
+
+			ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("TutorialTopic",
+					String.format(" SMS generado numer %d", i));
+			future.addCallback(new KafkaSendCallback<String, String>() {
+
+				@Override
+				public void onSuccess(SendResult<String, String> result) {
+					log.info("Mensaje enviado con exito {}", result.getRecordMetadata().offset());
+				}
+
+				@Override
+				public void onFailure(Throwable ex) {
+					log.info("Mensaje no enviado {}", ex);
+				}
+
+				@Override
+				public void onFailure(KafkaProducerException ex) {
+					log.info("Mensaje no enviado {}", ex);
+				}
+			});
+		}
+	}
+}
---
+++ src/main/java/com/iciencia/config/KafkaConfiguration.java
@@ -0,0 +1,76 @@
+package com.iciencia.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaConfiguration {
+
+	@Bean
+	public Map<String, Object> consumerProperties() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.86:9092");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "iciencia-group");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		return props;
+	}
+
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory() {
+		return new DefaultKafkaConsumerFactory<>(consumerProperties());
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		//Pemite recibir los mensajes en bash en grupo
+		factory.setBatchListener(true);
+		return factory;
+	}
+
+	
+	
+	// Producer
+	private Map<String, Object> producerProps() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.86:9092");
+		// G:Define los reintentos que se realizarán en caso de error
+		props.put(ProducerConfig.RETRIES_CONFIG, 0);
+		// El producer agrupará los registros en batches, mejorando el performance
+		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+		// Los batches se agruparan de acuerdo de un periodo de tiempo, está definido en
+		// milisegundos
+		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+		// Define el espacio de memoria que se asignará para colocar los mensajes que
+		// están pendientes por enviar
+		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		return props;
+	}
+
+	@Bean
+	public KafkaTemplate<String, String> createTemplate() {
+		Map<String, Object> senderProps = producerProps();
+		ProducerFactory<String, String> pf = new DefaultKafkaProducerFactory<>(senderProps);
+		KafkaTemplate<String, String> template = new KafkaTemplate<>(pf);
+		return template;
+	}
+
+}
